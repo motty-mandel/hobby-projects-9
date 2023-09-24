1952e54be8d0c1fc4720a25566550c6f22de1ba3
motty mandel <mandelmottyisrael65@gmail.com>
2023-09-24 11:17:06 -0400

    added associations for the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+});
 
 // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Tag, {
+//   through: {
+//     model: ProductTag,
+//     unique: false,
+//   },
+//   as: 'product_tag'
+// });
 
 // Tags belongToMany Products (through ProductTag)
+// Tag.belongToMany(Product, {
+//   through: {
+//     model: ProductTag,
+//     unique: false,
+//   },
+//   as: 'tagged_product'
+// });
 
 module.exports = {
   Product,
